d702d9cd5320c301a4278880429e41a6a6e3d333
iyreen@naver.com <117033332+hightruepotato@users.noreply.github.com>
2026-01-11 12:28:10 +0900

    4

---
+++ 2-2.py
@@ -0,0 +1,23 @@
+food = "Python's favorite food is perl"
+print(food)
+
+say = '"python is very easy." he says.'
+print(say)
+
+food2 = 'Python\'s favorite food is perl'
+say2 = "\"Python is very easy.\" he says."
+print(food2, say2)
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+multiline = '''
+Life is too short
+You need python
+'''
+print(multiline)
+
+head  = "Python"
+tail = " is fun!"
+print(head + tail)
+
