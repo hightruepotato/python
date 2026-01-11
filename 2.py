abffcc6895daf245fc4c5055d89e8c01c269fdf4
iyreen@naver.com <117033332+hightruepotato@users.noreply.github.com>
2026-01-11 11:26:05 +0900

    3

--- 2.py
+++ 2.py
@@ -14,3 +14,26 @@ print(a - b)
 print(a + b)
 
 print(a / b)
+
+a = 3
+b = 4
+print(a ** b)
+
+print(7 % 3)
+
+print(3 % 7)
+
+print(7 / 4)
+
+print(7 // 4)
+
+a = 1
+a = a + 1
+print(a)
+
+a += 1
+print(a)
+
+a = 1
+a -= 1
+print(a)
